97c2c3ec3fe2007ea81c6d074f114fd9554f0678
jcy <poiuytasr@gmail.com>
2021-10-19 10:24:29 +0900

    [25기 정찬영] Add & Refactor: 로그인 레이아웃 & 카카오 로그인 구현 - 레이아웃 작업 - 카카오 로그인 리소스 추가 - 카카오 로그인 구현 - 환경 변수 설정 - config 파일에 URL 추가 - 리팩토링 - 백엔드 통신 테스트 &  토큰 localStorage에 저장 - sider review 반영 - 멘토 리뷰 반영

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,9 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
+import styled from 'styled-components';
 
-class Modal extends Component {
-  render() {
-    return <div>Modal</div>;
-  }
+export default function Modal({ children, closeModal }) {
+  return (
+    <StyledModal>
+      <Header>
+        <span>Login</span>
+        <span onClick={closeModal}>X</span>
+      </Header>
+      <Content>{children}</Content>
+    </StyledModal>
+  );
 }
 
-export default Modal;
+const StyledModal = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  box-shadow: 2px 2px 6px 2px rgb(0 0 0 / 10%);
+`;
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 15px;
+  background-color: ${({ theme }) => theme.purple};
+  color: white;
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 20px 20px 0 20px;
+  background-color: #fff;
+`;
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,94 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import Modal from 'components/Modal';
+import { KAKAO_LOGIN_URL } from 'config';
 
-class Login extends Component {
-  render() {
-    return <div>Login page</div>;
-  }
+export default function Login() {
+  return (
+    <Modal>
+      <Input placeholder="아이디" />
+      <Input placeholder="비밀번호" />
+      <LineToSaveId>
+        <SaveId>
+          <input type="checkbox" />
+          <label>아이디 저장</label>
+        </SaveId>
+        <MobileSimpleLogin>
+          <label>휴대폰 간편 로그인</label>
+          <Ad>광고</Ad>
+        </MobileSimpleLogin>
+      </LineToSaveId>
+      <LoginBtn>로그인</LoginBtn>
+      <Links>
+        <Link to="/">ID/PW 찾기</Link>
+        <Link to="/">회원가입</Link>
+        <Link to="/">비회원 예매확인</Link>
+      </Links>
+      <a href={KAKAO_LOGIN_URL} target="_blank" rel="noopener noreferrer">
+        <KakaoButton src="images/kakao/kakao_login_large_wide.png" />
+      </a>
+    </Modal>
+  );
 }
+const SKY_BLUE = '#3d7db7';
 
-export default Login;
+const Input = styled.input`
+  height: 45px;
+  padding: 0px 10px;
+  margin-top: 15px;
+  border: 1px solid #d8d9db;
+  outline: none;
+`;
+
+const KakaoButton = styled.img`
+  height: 45px;
+  padding: 0px 10px;
+  margin: 30px 0;
+`;
+
+const LineToSaveId = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 15px 0 35px 0;
+  font-size: 13px;
+`;
+
+const SaveId = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const MobileSimpleLogin = styled.div`
+  display: flex;
+  align-items: center;
+  color: ${SKY_BLUE};
+`;
+
+const Ad = styled.em`
+  margin-left: 5px;
+  padding: 2px 4px;
+  border: 1px solid ${SKY_BLUE};
+`;
+
+const LoginBtn = styled.button`
+  witdh: 100%;
+  height: 40px;
+  padding: 0 30px;
+  border: none;
+  border-radius: 5px;
+`;
+
+const Links = styled.div`
+  width: 100%;
+  padding: 20px 0 30px 0;
+  text-align: center;
+
+  a {
+    color: #666;
+
+    &:not(:last-child) {
+      margin-right: 10px;
+    }
+  }
+`;
