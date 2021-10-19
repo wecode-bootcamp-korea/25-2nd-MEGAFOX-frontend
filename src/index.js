46bd8ba91a1e64e514cf51ddf612e9589cd8833e
jcy <poiuytasr@gmail.com>
2021-10-19 11:07:23 +0900

    [25기 정찬영] Add & Refactor: 폰트추가 & 리팩토링 - 기본 폰트 추가 - default value 수정 + 사용하지 않는 style 수정

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import GlobalStyle from 'styles/GlobalStyle';
+import GlobalFonts from 'styles/fonts/fonts';
 import { ThemeProvider } from 'styled-components';
 import Routes from 'Routes';
 import theme from 'styles/theme';
@@ -8,6 +9,7 @@ import theme from 'styles/theme';
 ReactDOM.render(
   <>
     <GlobalStyle />
+    <GlobalFonts />
     <ThemeProvider theme={theme}>
       <Routes />
     </ThemeProvider>
---
+++ src/styles/fonts/fonts.js
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  @font-face {
+    font-family: 'base';	
+    src: url('/fonts/NanumBarunGothic.ttf');
+  }
+`;
