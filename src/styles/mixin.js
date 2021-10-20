8f5a7b0ef1da4f657eb95748df0abe653dd8a035
Jetom88 <shinhro3o@gmail.com>
2021-10-20 20:06:16 +0900

    Modify: export 수정

---
+++ src/styles/mixin.js
@@ -0,0 +1,19 @@
+import { css } from 'styled-components';
+
+export const FlexCenter = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const FlexAround = css`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+export const FlexBetween = css`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
