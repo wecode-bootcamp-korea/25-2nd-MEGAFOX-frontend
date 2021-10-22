1f9f995a8cb038113ac9a3404b11b7fc88868cce
jcy <poiuytasr@gmail.com>
2021-10-22 17:34:52 +0900

    Fix: useFetch 수정

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,22 +1,22 @@
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
 import { API } from 'config';
 
 /**
  *
- * @param {string} path 해당 API 경로
- * @param {} reqInfo {headers, body, method = 'GET'}
- * @returns API를 통해 얻은 값
+ * @param {string} key 해당 API KEY
+ * @param {} options {headers, body, method = 'GET'}
+ * @returns API를 통해 얻은 데이터
  */
-export default function useFetch(path, options = {}) {
-  const [data, setData] = useState();
+export default function useFetch(key, options) {
+  const [data, setData] = useState({});
 
   useEffect(() => {
-    fetch(API + path, {
+    fetch(API[key], {
       ...options,
     })
       .then(res => res.json())
       .then(data => setData(data));
-  }, [path, options]);
+  }, [key, options]);
 
   return data;
 }
